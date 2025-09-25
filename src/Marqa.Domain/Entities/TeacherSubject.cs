9f31ba3e951504af387d89c50380b117f547ca08
Jamshid Maruf <90633296+JamshidMaruf@users.noreply.github.com>
2025-09-25 17:08:51 +0500

    added updates

---
+++ src/Marqa.Domain/Entities/TeacherSubject.cs
@@ -0,0 +1,10 @@
+namespace Marqa.Domain.Entities;
+
+public class TeacherSubject : Auditable
+{
+    public int TeacherId { get; set; }
+    public int SubjectId { get; set; }
+    
+    public Employee Teacher { get; set; }
+    public Subject Subject { get; set; }
+}
