9a1a13de601dbfc176ff3bc89b40b70aa50cfbb2
dilmurod-jabborov <futboluz825@gmail.com>
2025-10-02 11:59:41 +0500

    Fixed HomeTaskService

--- src/Marqa.Service/Services/HomeTask/Models/HomeTaskCreateModel.cs
+++ src/Marqa.Service/Services/HomeTask/Models/HomeTaskCreateModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Marqa.Service.Services.HomeTask.Models;
+﻿namespace Marqa.Service.Services.HomeTask.Models;
 public class HomeTaskCreateModel
 {
     public int LessonId { get; set; }
--- src/Marqa.Service/Services/HomeTask/Models/HomeTaskViewModel.cs
+++ src/Marqa.Service/Services/HomeTask/Models/HomeTaskViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace Marqa.Service.Services.HomeTask.Models;
-public class HomeTaskUpdateModel
+public class HomeTaskViewModel
 {
+    public int Id { get; set; }
     public int LessonId { get; set; }
     public DateTime Deadline { get; set; }
     public string Description { get; set; }
