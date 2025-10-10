f8fd500a017093437d135f01a1f28286f64631c8
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-10-10 16:25:31 +0500

    late time in lesson checkup has been modified to manual

--- src/Marqa.WebApi/Controllers/LessonsController.cs
+++ src/Marqa.WebApi/Controllers/LessonsController.cs
@@ -48,11 +48,11 @@ public class LessonsController(ILessonService lessonService) : Controller
     }
 
     [HttpPatch]
-    public async Task<IActionResult> PatchAsync(int Id, string name)
+    public async Task<IActionResult> PatchAsync(int id, string name)
     {
         try
         {
-            await lessonService.ModifyAsync(Id, name);
+            await lessonService.ModifyAsync(id, name);
 
             return Ok(new Response
             {
