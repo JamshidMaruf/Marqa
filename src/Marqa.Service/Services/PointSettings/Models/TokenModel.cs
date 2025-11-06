0e3c1ebd18db4d924eb51794b21f7f9601fcafc3
Zokirjon <tolqinovzokirjon347@gmail.com>
2025-11-06 16:48:04 +0500

    Bug fix/remove redundant configs (#118)
    * unneccessary parts removed
    
    * company service validators has been added
    
    * banner service bugs closed and banner entity modified
    
    * validation comfigurations for api projects has been added
    
    * libraries added to api layers

---
+++ src/Marqa.Service/Services/PointSettings/Models/TokenModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Marqa.Service.Services.PointSettings.Models;
+
+public class TokenModel
+{
+    public int PointSettingId { get; set; }
+    public int ActivationCount { get; set; }
+    public int Point { get; set; }
+    public int ExpirationTimeInHours { get; set; }
+}
