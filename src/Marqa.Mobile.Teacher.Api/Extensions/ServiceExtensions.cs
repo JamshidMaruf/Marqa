d2054e9c2207f2c9ee2c4b3f613d52af12662192
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-11-06 15:39:10 +0500

    validation comfigurations for api projects has been added

--- src/Marqa.Mobile.Teacher.Api/Extensions/ServiceExtensions.cs
+++ src/Marqa.Mobile.Teacher.Api/Extensions/ServiceExtensions.cs
@@ -3,6 +3,7 @@ using Marqa.DataAccess.Repositories;
 using Marqa.DataAccess.UnitOfWork;
 using Marqa.Service.Services.Auth;
 using Marqa.Service.Services.Settings;
+using Marqa.Service.Validators.Companies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -18,6 +19,7 @@ public static class ServicesExtension
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<ISettingService, SettingService>();
         services.AddScoped<IEncryptionService, EncryptionService>();
+        services.AddValidatorsFromAssemblyContaining<CompanyCreateModelValidator>();
     }
 
     public static void AddJWTService(this IServiceCollection services, IConfiguration configuration)
