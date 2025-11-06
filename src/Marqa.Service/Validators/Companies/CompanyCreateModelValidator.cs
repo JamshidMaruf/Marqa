73e811ea5101565583828fe13591b695e92a09cc
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-11-06 09:40:13 +0500

    company service validators has been added

---
+++ src/Marqa.Service/Validators/Companies/CompanyCreateModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Marqa.Service.Services.Companies.Models;
+
+namespace Marqa.Service.Validators.Companies;
+
+public class CompanyCreateModelValidator : AbstractValidator<CompanyCreateModel>
+{
+    public CompanyCreateModelValidator()
+    {
+        RuleFor(c => c.Name).NotNull().NotEmpty();
+    }
+}
