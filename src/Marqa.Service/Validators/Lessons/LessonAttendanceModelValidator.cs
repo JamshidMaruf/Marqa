9f550a64bf0dbbec758cefb361be302bd6c72f06
mukha_0 <muhammadali8261@mail.ru>
2025-11-08 14:21:59 +0500

    Added Validators for Lesson and HomeTask (#137)

---
+++ src/Marqa.Service/Validators/Lessons/LessonAttendanceModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Marqa.Service.Services.Lessons.Models;
+
+namespace Marqa.Service.Validators.Lessons;
+public class LessonAttendanceModelValidator : AbstractValidator<LessonAttendanceModel>
+{
+    public LessonAttendanceModelValidator()
+    {
+        RuleFor(x => x.LessonId).GreaterThan(0);
+        RuleFor(x => x.StudentId).GreaterThan(0);
+        RuleFor(x => x.LateTimeInMinutes).InclusiveBetween(0, 60);
+        RuleFor(x => x.Status).NotNull().IsInEnum();
+    }
+}
