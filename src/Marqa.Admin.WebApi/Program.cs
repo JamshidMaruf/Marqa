b23919c4962d2fbbe1f7f0958e1ad145c35f2db0
Zokirjon <tolqinovzokirjon347@gmail.com>
2025-11-08 16:27:07 +0500

    api layer errors has been solved (#141)

--- src/Marqa.Admin.WebApi/Program.cs
+++ src/Marqa.Admin.WebApi/Program.cs
@@ -1,35 +1,9 @@
-﻿using System.Text;
-using FluentValidation;
-using Marqa.Admin.WebApi.Extensions;
+﻿using Marqa.Admin.WebApi.Extensions;
 using Marqa.Admin.WebApi.Middlewares;
 using Marqa.DataAccess.Contexts;
-using Marqa.DataAccess.Repositories;
-using Marqa.DataAccess.UnitOfWork;
 using Marqa.Service.Helpers;
-using Marqa.Service.Servcies.Products;
-using Marqa.Service.Services.Auth;
-using Marqa.Service.Services.Companies;
-using Marqa.Service.Services.Courses;
-using Marqa.Service.Services.EmployeeRoles;
-using Marqa.Service.Services.EmployeeRoles.Models;
-using Marqa.Service.Services.Employees;
-using Marqa.Service.Services.Exams;
-using Marqa.Service.Services.Files;
-using Marqa.Service.Services.HomeTasks;
-using Marqa.Service.Services.Lessons;
-using Marqa.Service.Services.PointSettings;
-using Marqa.Service.Services.Products;
-using Marqa.Service.Services.Ratings;
-using Marqa.Service.Services.Settings;
-using Marqa.Service.Services.StudentPointHistories;
-using Marqa.Service.Services.Students;
-using Marqa.Service.Services.Subjects;
-using Marqa.Service.Validators.Companies;
-using Marqa.Service.Validators.EmployeeRoles;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -37,8 +11,6 @@ builder.Services.AddLogging();
 
 builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
-
-// Add services to the container.
 
 builder.Services.AddControllers();
 
@@ -49,20 +21,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(option 
     => option.UseNpgsql(builder.Configuration.GetConnectionString("PostgresSQLConnection")));
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["JWT:Issuer"],
-            ValidAudience = builder.Configuration["JWT:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
-        };
-    });
+builder.Services.AddMarqaServices();
+
+builder.Services.AddJWTService();
 
 builder.Services.AddControllers(options =>
 {
@@ -70,32 +31,7 @@ builder.Services.AddControllers(options =>
         new LowerCaseControllerName()));
 });
 
-
 builder.Services.AddAuthorization();
-
-builder.Services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<ICompanyService, CompanyService>();
-builder.Services.AddScoped<ICourseService, CourseService>();
-builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-builder.Services.AddScoped<IEmployeeRoleService, EmployeeRoleService>();
-builder.Services.AddScoped<IHomeTaskService, HomeTaskService>();
-builder.Services.AddScoped<IFileService, FileService>();
-builder.Services.AddScoped<ILessonService, LessonService>();
-builder.Services.AddScoped<IStudentService, StudentService>();
-builder.Services.AddScoped<ISubjectService, SubjectService>();
-builder.Services.AddScoped<IPointSettingService,  PointSettingService>();
-builder.Services.AddScoped<IExamService, ExamService>();
-builder.Services.AddScoped<IRatingService, RatingService>();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IStudentPointHistoryService, StudentPointHistoryService>();
-builder.Services.AddValidatorsFromAssemblyContaining<CompanyCreateModelValidator>();
-builder.Services.AddScoped<IValidator<EmployeeRoleCreateModel>, EmployeeRoleCreateModelValidator>();
-builder.Services.AddScoped<IValidator<EmployeeRoleUpdateModel>, EmployeeRoleUpdateModelValidator>();
-
-builder.Services.AddScoped<IEncryptionService, EncryptionService>();
-builder.Services.AddScoped<ISettingService, SettingService>();
 
 var app = builder.Build();
 
