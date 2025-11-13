7c55c64ef9803697d980927c7f1fefbf7645943c
mukha_0 <muhammadali8261@mail.ru>
2025-11-13 17:50:55 +0500

    Added validators for Subjects,Settings,Orders (#158)

---
+++ src/Marqa.Service/Validators/Subjects/SubjectCreateModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Marqa.Service.Services.Subjects.Models;
+
+namespace Marqa.Service.Validators.Subjects;
+public class SubjectCreateModelValidator : AbstractValidator<SubjectCreateModel>
+{
+    public SubjectCreateModelValidator()
+    {
+        RuleFor(x => x.CompanyId).GreaterThan(0);
+        RuleFor(x => x.Name)
+           .NotEmpty().WithMessage("Name cannot be empty.")
+           .MaximumLength(100).WithMessage("Name length must not exceed word limit.");
+    }
+}
---
+++ src/Marqa.Service/Validators/Subjects/SubjectUpdateModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Marqa.Service.Services.Subjects.Models;
+
+namespace Marqa.Service.Validators.Subjects;
+public class SubjectUpdateModelValidator : AbstractValidator<SubjectUpdateModel>
+{
+    public SubjectUpdateModelValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name cannot be empty.")
+            .MaximumLength(100).WithMessage("Name length must not exceed word limit.");
+    }
+}
