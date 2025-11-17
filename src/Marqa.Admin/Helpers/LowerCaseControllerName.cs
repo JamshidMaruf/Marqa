675493e765c923d3f6e838d77ebe8a15b21eee74
Zokirjon <tolqinovzokirjon347@gmail.com>
2025-11-17 15:30:14 +0500

    mvc admin layer configured (#167)

---
+++ src/Marqa.Admin/Helpers/LowerCaseControllerName.cs
@@ -0,0 +1,11 @@
+﻿using Marqa.Shared.Helpers;
+
+namespace Marqa.Admin.Extensions;
+
+public class LowerCaseControllerName : IOutboundParameterTransformer
+{
+    public string? TransformOutbound(object? value)
+    {
+        return LowerCaseConverter.Convert(value);
+    }
+}
