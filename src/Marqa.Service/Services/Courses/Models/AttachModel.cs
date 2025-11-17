db0a3bdc44b4e3574028e4944c78ca232f446116
Zokirjon <tolqinovzokirjon347@gmail.com>
2025-11-17 13:14:11 +0500

    employee role attaching logic bug in employee create and update methods has been corrected (#166)

---
+++ src/Marqa.Service/Services/Courses/Models/AttachModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Marqa.Service.Services.Courses.Models;
+
+public class AttachModel
+{
+    public int CourseId { get; set; }
+    public int StudentId { get; set; }
+}
---
+++ src/Marqa.Service/Services/Courses/Models/DetachModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Marqa.Service.Services.Courses.Models;
+
+public class DetachModel
+{
+    public int CourseId { get; set; }
+    public int StudentId { get; set; }
+}
