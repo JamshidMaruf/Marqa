c0112c16c804efb43df485405fdc0021f4690392
Jamshid Maruf <90633296+JamshidMaruf@users.noreply.github.com>
2025-11-20 17:54:21 +0500

    Feature/updates (#173)
    * Added changes
    
    * Added changes

--- src/Marqa.Admin/Controllers/SettingsController.cs
+++ src/Marqa.Admin/Controllers/SettingsController.cs
@@ -1,6 +1,13 @@
+using Marqa.Service.Services.Settings;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Marqa.Admin.Controllers;
 
-public class SettingsController
+public class SettingsController(ISettingService settingService) : Controller
 {
-    
+    public async Task<IActionResult> Index()
+    {
+        var result = await settingService.GetAllAsync();
+        return View(result);
+    }
 }
