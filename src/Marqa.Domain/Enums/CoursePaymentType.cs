fb1b8f6d42041b35f1934d9ec547aa0ab42c374a
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-11-30 15:31:39 +0500

    attach student to a new course feature has been updated

---
+++ src/Marqa.Domain/Enums/CoursePaymentType.cs
@@ -0,0 +1,8 @@
+﻿namespace Marqa.Domain.Enums;
+
+public enum CoursePaymentType
+{
+    Fixed = 1,
+    DiscountFree = 2,
+    DiscountInPercentage = 3
+}
--- src/Marqa.Service/Services/Courses/Models/AttachModel.cs
+++ src/Marqa.Service/Services/Courses/Models/AttachModel.cs
@@ -1,7 +1,12 @@
-﻿namespace Marqa.Service.Services.Courses.Models;
+﻿using Marqa.Domain.Enums;
+
+namespace Marqa.Service.Services.Courses.Models;
 
 public class AttachModel
 {
     public int CourseId { get; set; }
     public int StudentId { get; set; }
+    public CoursePaymentType PaymentType { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime EnrollmentDate { get; set;
 }
