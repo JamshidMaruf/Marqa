5532f57bb47d8ac20af2cdd05035c94fcbf246d1
hasankhon007 <savriddinovhasanxon007@gmail.com>
2025-11-30 10:54:01 +0500

    Replaced ApiResponse with Response

--- src/Marqa.Admin.WebApi/Controllers/EnumsController.cs
+++ src/Marqa.Admin.WebApi/Controllers/EnumsController.cs
@@ -23,27 +23,47 @@ public class EnumsController : ControllerBase
     public async Task<IActionResult> GetGenders()
     {
         var result = _enumService.GetEnumValues<Gender>();
-        return Ok(new ApiResponse<List<EnumGetModel>>(result));
+        return Ok(new Response<List<EnumGetModel>>
+        {
+            StatusCode = 200,
+            Message = "success",
+            Data = result
+        });
     }
 
     [HttpGet("report-periods")]
     public async Task<IActionResult> GetReportPeriods()
     {
         var result = _enumService.GetEnumValues<ReportPeriod>();
-        return Ok(new ApiResponse<List<EnumGetModel>>(result));
+        return Ok(new Response<List<EnumGetModel>>
+        {
+            StatusCode = 200,
+            Message = "success",
+            Data = result
+        });
     }
 
     [HttpGet("payment-operation-types")]
     public async Task<IActionResult> GetPaymentOperationTypes()
     {
         var result = _enumService.GetEnumValues<PaymentOperationType>();
-        return Ok(new ApiResponse<List<EnumGetModel>>(result));
+        return Ok(new Response<List<EnumGetModel>>
+        {
+            StatusCode = 200,
+            Message = "success",
+            Data = result
+        });
     }
 
     [HttpGet("employee-statuses")]
     public async Task<IActionResult> GetEmployeeStatuses()
     {
         var result = _enumService.GetEnumValues<EmployeeStatus>();
-        return Ok(new ApiResponse<List<EnumGetModel>>(result));
+        return Ok(new Response<List<EnumGetModel>>
+        { 
+            StatusCode = 200,
+            Message = "success",
+            Data = result 
+        });
     }
 }
