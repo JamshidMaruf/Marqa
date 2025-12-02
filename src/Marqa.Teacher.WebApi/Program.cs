e27eab33bbb5051d2057e48f60cd9259defb2b83
mukha_0 <muhammadali8261@mail.ru>
2025-12-02 20:16:43 +0500

    test push

--- src/Marqa.Teacher.WebApi/Program.cs
+++ src/Marqa.Teacher.WebApi/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerService();
 
+
 builder.Services.AddDbContext<AppDbContext>(option
     => option.UseNpgsql(builder.Configuration.GetConnectionString("PostgresSQLConnection")));
 
