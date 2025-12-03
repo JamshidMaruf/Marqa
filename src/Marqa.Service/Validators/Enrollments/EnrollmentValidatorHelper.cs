021676e0c176f21f2b8f81d88e1e6cdae03f4b1b
M1 <sharobiddinovm8894@gmail.com>
2025-12-03 11:16:45 +0500

    Feature/add validator for enrollement (#210)
    * Added converter UTC -> Local Time (#209)
    
    * bosh joy
    
    ---------
    
    Co-authored-by: mukha_0 <muhammadali8261@mail.ru>

---
+++ src/Marqa.Service/Validators/Enrollments/EnrollmentValidatorHelper.cs
@@ -0,0 +1,35 @@
+﻿using Marqa.DataAccess.UnitOfWork;
+using Marqa.Domain.Enums;
+using Marqa.Service.Services.Enrollments.Models;
+
+public static class EnrollmentValidatorHelper
+{
+    public static bool ValidateAmount(EnrollmentCreateModel model, decimal amount)
+    {
+        if (model.PaymentType == CoursePaymentType.DiscountInPercentage)
+            return amount >= 0 && amount <= 100;
+
+        if (model.PaymentType == CoursePaymentType.Fixed)
+            return amount >= 0;
+
+        return true;
+    }
+
+    public static async Task<bool> ValidateCourseCapacityAsync(IUnitOfWork unitOfWork, int courseId, int studentId)
+    {
+        var course = await unitOfWork.Courses.SelectAsync(c => c.Id == courseId);
+
+        if (course == null)
+            return false;
+
+        if (course.MaxStudentCount <= course.EnrolledStudentCount)
+            return false;
+
+        var existingEnrollment = await unitOfWork.Enrollments.CheckExistAsync(
+            e => e.StudentId == studentId && e.CourseId == courseId);
+
+        return !existingEnrollment;
+
+    }
+}
+
