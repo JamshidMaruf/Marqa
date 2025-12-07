723b2f292c376d075cb3ec376ddbe724b77fdda6
Zokirjon <tolqinovzokirjon347@gmail.com>
2025-12-07 15:29:56 +0500

    Update TeacherPaymentType.cs

--- src/Marqa.Domain/Enums/TeacherPaymentType.cs
+++ src/Marqa.Domain/Enums/TeacherPaymentType.cs
@@ -4,15 +4,15 @@ namespace Marqa.Domain.Enums;
 
 public enum TeacherPaymentType
 {
-    [Display(Name = "Fixed", Description = "Qat'iy oylik har oyda o'zgarmaydigan belgilangan summa")]
+    [Description("Qat'iy oylik")]
     Fixed = 1,
 
-    [Display(Name = "Percentage", Description = "Foiz yutuq miqdoriga asosan to'langan foiz")]
+    [Description("Foiz (o'quvchilardan)")]
     Percentage = 2,
 
-    [Display(Name = "Hourly", Description = "Soatlik har bir soat uchun to'langan oylik")]
+    [Description("Soatlik")]
     Hourly = 3,
 
-    [Display(Name = "Mixed", Description = "Aralash qat'iy oylik + foiz kombinatsiyasi")]
+    [Description("Aralash (oylik + foiz")]
     Mixed = 4
 }
