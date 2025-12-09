8d073ffd3f9517b7a492edcc97fefc6eee00d287
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-12-09 16:34:30 +0500

    teacher salary related changes

---
+++ src/Marqa.Service/Services/Teachers/Models/CalculatedTeacherSalaryModel.cs
@@ -0,0 +1,21 @@
+﻿using Marqa.Domain.Enums;
+
+namespace Marqa.Service.Services.Teachers.Models;
+
+public class CalculatedTeacherSalaryModel
+{
+    public int GroupsCount { get; set; }
+    public int StudentsCount { get; set; }
+    public decimal? Percent { get; set; }
+    public decimal? Amount { get; set; }
+    public decimal Total { get; set; }
+    public TeacherPaymentType PaymentType { get; set; }
+    public List<GroupData> Groups { get; set; }
+
+    public class GroupData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
