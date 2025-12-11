9502bbf695b166054051c8c2aea5ad71716d0014
SharobiddinovM1 <sharobiddinovm8894@gmail.com>
2025-12-11 10:23:10 +0500

    Improve structure: grouped related repositories and sorted alphabetically

--- src/Marqa.DataAccess/UnitOfWork/IUnitOfWork.cs
+++ src/Marqa.DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -7,95 +7,64 @@ namespace Marqa.DataAccess.UnitOfWork;
 
 public interface IUnitOfWork : IDisposable
 {
+    // 1. Management and Settings
     IRepository<Banner> Banners { get; }
-
     IRepository<Company> Companies { get; }
-
-    IRepository<Course> Courses { get; }
-
-    IRepository<CourseWeekday> CourseWeekdays { get; }
-
-    IRepository<Employee> Employees { get; }
-
-    IRepository<EmployeeRole> EmployeeRoles { get; }
-
-    IRepository<Student> Students { get; }
-
-    IRepository<StudentDetail> StudentDetails { get; }
-
-    IRepository<Exam> Exams { get; }
-
-    IRepository<HomeTask> HomeTasks { get; }
-
-    IRepository<Lesson> Lessons { get; }
-    IRepository<EmployeePaymentOperation> EmployeePaymentOperations { get; }
-
-    IRepository<LessonAttendance> LessonAttendances { get; }
-
-    IRepository<OTP> OTPs { get; }
-
-    IRepository<Enrollment> Enrollments { get; }
-    IRepository<Expense> Expenses { get; }
-    IRepository<ExpenseCategory> ExpenseCategories { get; }
-
-    IRepository<StudentExamResult> StudentExamResults { get; }
-
-    IRepository<Subject> Subjects { get; }
-
-    IRepository<StudentHomeTask> StudentHomeTasks { get; }
-
-    IRepository<TeacherSubject> TeacherSubjects { get; }
-
-    IRepository<LessonFile> LessonFiles { get; }
-
-    IRepository<LessonVideo> LessonVideos { get; }
-
-    IRepository<StudentHomeTaskFile> StudentHomeTaskFiles { get; }
-
-    IRepository<HomeTaskFile> HomeTaskFiles { get; }
-
-    IRepository<StudentPointHistory> StudentPointHistories { get; }
-
-    IRepository<PointSystemSetting> PointSystemSettings { get; }
-
-    IRepository<PointSetting> PointSettings { get; }
-
-    IRepository<Product> Products { get; }
-
-    IRepository<Order> Orders { get; }
-
-    IRepository<OrderItem> OrderItems { get; }
-
+    IRepository<Permission> Permissions { get; }
+    IRepository<RolePermission> RolePermissions { get; }
     IRepository<Setting> Settings { get; }
 
-    IRepository<Basket> Baskets { get; }
-
-    IRepository<BasketItem> BasketItems { get; }
-
-    IRepository<ExamSetting> ExamSettings { get; }
-
-    IRepository<ExamSettingItem> ExamSettingItems { get; }
-
-    IRepository<Permission> Permissions { get; }
-
-    IRepository<RolePermission> RolePermissions { get; }
-
+    // 2. People and Personal Information
+    IRepository<Employee> Employees { get; }
+    IRepository<EmployeeRole> EmployeeRoles { get; }
     IRepository<RefreshToken> RefreshTokens { get; }
-
-    IRepository<StudentPaymentOperation> StudentPaymentOperations { get; }
-
+    IRepository<Student> Students { get; }
+    IRepository<StudentDetail> StudentDetails { get; }
+    IRepository<Teacher> Teachers { get; }
     IRepository<User> Users { get; }
 
+    // 3. Courses and Learning Process
+    IRepository<Course> Courses { get; }
+    IRepository<CourseWeekday> CourseWeekdays { get; }
+    IRepository<Enrollment> Enrollments { get; }
+    IRepository<Exam> Exams { get; }
+    IRepository<Lesson> Lessons { get; }
+    IRepository<LessonAttendance> LessonAttendances { get; }
+    IRepository<Subject> Subjects { get; }
+    IRepository<TeacherSubject> TeacherSubjects { get; }
+
+    // 4. Learning Components and Materials
     IRepository<Asset> Assets { get; }
+    IRepository<ExamSetting> ExamSettings { get; }
+    IRepository<ExamSettingItem> ExamSettingItems { get; }
+    IRepository<HomeTask> HomeTasks { get; }
+    IRepository<HomeTaskFile> HomeTaskFiles { get; }
+    IRepository<LessonFile> LessonFiles { get; }
+    IRepository<LessonVideo> LessonVideos { get; }
+    IRepository<OTP> OTPs { get; }
+    IRepository<StudentExamResult> StudentExamResults { get; }
+    IRepository<StudentHomeTask> StudentHomeTasks { get; }
+    IRepository<StudentHomeTaskFile> StudentHomeTaskFiles { get; }
 
-    IRepository<EnrollmentFrozen> EnrollmentFrozens { get; }
-
+    // 5. Enrollment and Payment Operations
+    IRepository<Basket> Baskets { get; }
+    IRepository<BasketItem> BasketItems { get; }
+    IRepository<EmployeePaymentOperation> EmployeePaymentOperations { get; }
     IRepository<EnrollmentCancellation> EnrollmentCancellations { get; }
-
+    IRepository<EnrollmentFrozen> EnrollmentFrozens { get; }
     IRepository<EnrollmentTransfer> EnrollmentTransfers { get; }
-
-    IRepository<Teacher> Teachers { get; }
+    IRepository<StudentPaymentOperation> StudentPaymentOperations { get; }
     IRepository<TeacherPaymentOperation> TeacherPaymentOperations { get; }
+
+    // 6. Points, Pricing and Financial System
+    IRepository<Expense> Expenses { get; }
+    IRepository<ExpenseCategory> ExpenseCategories { get; }
+    IRepository<Order> Orders { get; }
+    IRepository<OrderItem> OrderItems { get; }
+    IRepository<PointSetting> PointSettings { get; }
+    IRepository<PointSystemSetting> PointSystemSettings { get; }
+    IRepository<Product> Products { get; }
+    IRepository<StudentPointHistory> StudentPointHistories { get; }
 
     Task SaveAsync();
     Task<IDbContextTransaction> BeginTransactionAsync();
--- src/Marqa.DataAccess/UnitOfWork/UnitOfWork.cs
+++ src/Marqa.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -7,55 +7,65 @@ namespace Marqa.DataAccess.UnitOfWork;
 
 public class UnitOfWork(AppDbContext context) : IUnitOfWork
 {
+    // 1. Management and Settings
     public IRepository<Banner> Banners { get; } = new Repository<Banner>(context);
     public IRepository<Company> Companies { get; } = new Repository<Company>(context);
-    public IRepository<Course> Courses { get; } = new Repository<Course>(context);
-    public IRepository<CourseWeekday> CourseWeekdays { get; } = new Repository<CourseWeekday>(context);
+    public IRepository<Permission> Permissions { get; } = new Repository<Permission>(context);
+    public IRepository<RolePermission> RolePermissions { get; } = new Repository<RolePermission>(context);
+    public IRepository<Setting> Settings { get; } = new Repository<Setting>(context);
+    
+    // 2. People and Personal Information
     public IRepository<Employee> Employees { get; } = new Repository<Employee>(context);
     public IRepository<EmployeeRole> EmployeeRoles { get; } = new Repository<EmployeeRole>(context);
+    public IRepository<RefreshToken> RefreshTokens { get; } = new Repository<RefreshToken>(context);
     public IRepository<Student> Students { get; } = new Repository<Student>(context);
     public IRepository<StudentDetail> StudentDetails { get; } = new Repository<StudentDetail>(context);
+    public IRepository<Teacher> Teachers { get; } = new Repository<Teacher>(context);
+    public IRepository<User> Users { get; } = new Repository<User>(context);
+    
+    // 3. Courses and Learning Process
+    public IRepository<Course> Courses { get; } = new Repository<Course>(context);
+    public IRepository<CourseWeekday> CourseWeekdays { get; } = new Repository<CourseWeekday>(context);
+    public IRepository<Enrollment> Enrollments { get; } = new Repository<Enrollment>(context);
     public IRepository<Exam> Exams { get; } = new Repository<Exam>(context);
-    public IRepository<HomeTask> HomeTasks { get; } = new Repository<HomeTask>(context);
     public IRepository<Lesson> Lessons { get; } = new Repository<Lesson>(context);
     public IRepository<LessonAttendance> LessonAttendances { get; } = new Repository<LessonAttendance>(context);
-    public IRepository<OTP> OTPs { get; } = new Repository<OTP>(context);
-    public IRepository<Enrollment> Enrollments { get; } = new Repository<Enrollment>(context);
-    public IRepository<Expense> Expenses { get; }
-    public IRepository<ExpenseCategory> ExpenseCategories { get; }
-    public IRepository<StudentExamResult> StudentExamResults { get; } = new Repository<StudentExamResult>(context);
     public IRepository<Subject> Subjects { get; } = new Repository<Subject>(context);
-    public IRepository<StudentHomeTask> StudentHomeTasks { get; } = new Repository<StudentHomeTask>(context);
     public IRepository<TeacherSubject> TeacherSubjects { get; } = new Repository<TeacherSubject>(context);
+    
+    // 4. Learning Components and Materials
+    public IRepository<Asset> Assets { get; } = new Repository<Asset>(context);
+    public IRepository<ExamSetting> ExamSettings { get; } = new Repository<ExamSetting>(context);
+    public IRepository<ExamSettingItem> ExamSettingItems { get; } = new Repository<ExamSettingItem>(context);
+    public IRepository<HomeTask> HomeTasks { get; } = new Repository<HomeTask>(context);
+    public IRepository<HomeTaskFile> HomeTaskFiles { get; } = new Repository<HomeTaskFile>(context);
     public IRepository<LessonFile> LessonFiles { get; } = new Repository<LessonFile>(context);
     public IRepository<LessonVideo> LessonVideos { get; } = new Repository<LessonVideo>(context);
+    public IRepository<OTP> OTPs { get; } = new Repository<OTP>(context);
+    public IRepository<StudentExamResult> StudentExamResults { get; } = new Repository<StudentExamResult>(context);
+    public IRepository<StudentHomeTask> StudentHomeTasks { get; } = new Repository<StudentHomeTask>(context);
     public IRepository<StudentHomeTaskFile> StudentHomeTaskFiles { get; } = new Repository<StudentHomeTaskFile>(context);
-    public IRepository<HomeTaskFile> HomeTaskFiles { get; } = new Repository<HomeTaskFile>(context);
-    public IRepository<StudentPointHistory> StudentPointHistories { get; } = new Repository<StudentPointHistory>(context);
-    public IRepository<PointSystemSetting> PointSystemSettings { get; } = new Repository<PointSystemSetting>(context);
-    public IRepository<PointSetting> PointSettings { get; } = new Repository<PointSetting>(context);
-    public IRepository<Product> Products { get; } = new Repository<Product>(context);
-    public IRepository<Order> Orders { get; } = new Repository<Order>(context);
-    public IRepository<OrderItem> OrderItems { get; } = new Repository<OrderItem>(context);
-    public IRepository<StudentPaymentOperation> StudentPaymentOperations { get; } = new Repository<StudentPaymentOperation>(context);
-    public IRepository<Setting> Settings { get; } = new Repository<Setting>(context);
+    
+    // 5. Enrollment and Payment Operations
     public IRepository<Basket> Baskets { get; } = new Repository<Basket>(context);
     public IRepository<BasketItem> BasketItems { get; } = new Repository<BasketItem>(context);
-    public IRepository<ExamSetting> ExamSettings { get; } = new Repository<ExamSetting>(context);
     public IRepository<EmployeePaymentOperation> EmployeePaymentOperations { get; } = new Repository<EmployeePaymentOperation>(context);
-    public IRepository<ExamSettingItem> ExamSettingItems { get; } = new Repository<ExamSettingItem>(context);
-    public IRepository<Permission> Permissions { get; }  = new Repository<Permission>(context);
-    public IRepository<RolePermission> RolePermissions { get; } = new Repository<RolePermission>(context);
-    public IRepository<RefreshToken> RefreshTokens { get; } = new Repository<RefreshToken>(context);
-    public IRepository<User> Users { get; } = new Repository<User>(context);
-    public IRepository<Asset> Assets { get; } = new Repository<Asset>(context);
-    public IRepository<EnrollmentFrozen> EnrollmentFrozens => new Repository<EnrollmentFrozen>(context);
-    public IRepository<EnrollmentCancellation> EnrollmentCancellations => new Repository<EnrollmentCancellation>(context);
-    public IRepository<EnrollmentTransfer> EnrollmentTransfers => new Repository<EnrollmentTransfer>(context);
-    public IRepository<Teacher> Teachers => new Repository<Teacher>(context);
-    public IRepository<TeacherPaymentOperation> TeacherPaymentOperations { get; }
-
+    public IRepository<EnrollmentCancellation> EnrollmentCancellations { get; } = new Repository<EnrollmentCancellation>(context);
+    public IRepository<EnrollmentFrozen> EnrollmentFrozens { get; } = new Repository<EnrollmentFrozen>(context);
+    public IRepository<EnrollmentTransfer> EnrollmentTransfers { get; } = new Repository<EnrollmentTransfer>(context);
+    public IRepository<StudentPaymentOperation> StudentPaymentOperations { get; } = new Repository<StudentPaymentOperation>(context);
+    public IRepository<TeacherPaymentOperation> TeacherPaymentOperations { get; } // TODO: Add implementation
     
+    // 6. Points, Pricing and Financial System
+    public IRepository<Expense> Expenses { get; } // TODO: Add implementation
+    public IRepository<ExpenseCategory> ExpenseCategories { get; } // TODO: Add implementation
+    public IRepository<Order> Orders { get; } = new Repository<Order>(context);
+    public IRepository<OrderItem> OrderItems { get; } = new Repository<OrderItem>(context);
+    public IRepository<PointSetting> PointSettings { get; } = new Repository<PointSetting>(context);
+    public IRepository<PointSystemSetting> PointSystemSettings { get; } = new Repository<PointSystemSetting>(context);
+    public IRepository<Product> Products { get; } = new Repository<Product>(context);
+    public IRepository<StudentPointHistory> StudentPointHistories { get; } = new Repository<StudentPointHistory>(context);
+
     public async Task SaveAsync()
     {
         await context.SaveChangesAsync();
