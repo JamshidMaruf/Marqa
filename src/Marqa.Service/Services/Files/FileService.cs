dacb27c1e78071fb8638eb059bdcd2e2a1dc9440
SharobiddinovM1 <sharobiddinovm8894@gmail.com>
2025-12-15 20:59:07 +0500

    Add extension checker methods for image and video files

--- src/Marqa.Service/Services/Files/FileService.cs
+++ src/Marqa.Service/Services/Files/FileService.cs
@@ -8,19 +8,27 @@ public class FileService : IFileService
 {
     public async Task<(string FileName, string FilePath)> UploadAsync(IFormFile file, string folder)
     {
-        if (file == null || file.Length <= 0)
-            throw new ArgumentIsNotValidException("File not upload");
-        
+        if (file is null || file.Length <= 0)
+            throw new ArgumentIsNotValidException("File not uploaded");
+
+        var extension = Path.GetExtension(file.FileName);
         var rootPath = Path.Combine(EnvironmentHelper.WebRootPath, folder);
+
         if (!Directory.Exists(rootPath))
             Directory.CreateDirectory(rootPath);
 
-        var fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}" ;
+        var fileName = $"{Guid.NewGuid()}{extension}";
         var fullPath = Path.Combine(rootPath, fileName);
-        
+
         await using var stream = new FileStream(fullPath, FileMode.Create);
         await file.CopyToAsync(stream);
 
         return (fileName, fullPath);
     }
+
+    public bool IsImageExtension(string extension) =>
+        FileExtensionHelper.IsImageExtension(extension);
+
+    public bool IsVideoExtension(string extension) =>
+        FileExtensionHelper.IsVideoExtension(extension);
 }
--- src/Marqa.Service/Services/Files/IFileService.cs
+++ src/Marqa.Service/Services/Files/IFileService.cs
@@ -1,8 +1,9 @@
-using Microsoft.AspNetCore.Http;
-
-namespace Marqa.Service.Services.Files;
+﻿using Microsoft.AspNetCore.Http;
 
 public interface IFileService
 {
     Task<(string FileName, string FilePath)> UploadAsync(IFormFile file, string folder);
+
+    bool IsImageExtension(string extension);
+    bool IsVideoExtension(string extension);
 }
