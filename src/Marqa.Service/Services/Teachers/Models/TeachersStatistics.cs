d6d126c699a23c58abac1e9ee75cf600f79614ba
Zokirjon <tolqinovzokirjon347@gmail.com>
2025-12-21 19:22:22 +0500

    Rename TotalInactiveTeacherCount to TotalLeftTeacherCount

--- src/Marqa.Service/Services/Teachers/Models/TeachersStatistics.cs
+++ src/Marqa.Service/Services/Teachers/Models/TeachersStatistics.cs
@@ -6,6 +6,6 @@
     public int TotalLeadTeachersCount { get; set; }
     public int TotalAssistantTeachersCount { get; set; }
     public int TotalActiveTeacherCount { get; set; }
-    public int TotalInactiveTeacherCount { get; set; }
+    public int TotalLeftTeacherCount { get; set; }
     public int TotalOnLeaveTeacherCount { get; set; }
 }
