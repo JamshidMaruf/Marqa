18f8c7139341d204165048d97640b7bc73fe3dc8
Hasanxon <155837160+hasankhon007@users.noreply.github.com>
2025-12-26 19:21:24 +0500

    Added new method (#313)

---
+++ src/Marqa.Service/Services/Lessons/Models/CourseLesson.cs
@@ -0,0 +1,15 @@
+﻿namespace Marqa.Service.Services.Lessons.Models;
+
+public class CourseLesson
+{
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+    public int TeacherId { get; set; }
+    public string TeacherName { get; set; }
+    public int CourseStudentsCount { get; set; }
+    public int CoursePresentStudentsCount { get; set; }
+    public decimal AttendPercentage { get; set; }
+    public bool IsCheckedUp{ get; set; }
+    public int LessonNumber { get; set; }
+    public int LessonId{ get; set; }
+}
