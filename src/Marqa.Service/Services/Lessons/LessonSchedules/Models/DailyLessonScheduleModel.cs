7a3ce6c13802ca8c3512ef8788b0d6a84c9139cb
Hasanxon <155837160+hasankhon007@users.noreply.github.com>
2025-12-30 07:56:15 +0500

    Added schedule method (#321)

---
+++ src/Marqa.Service/Services/Lessons/LessonSchedules/Models/DailyLessonScheduleModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Marqa.Service.Services.Lessons.LessonSchedules.Models;
+
+public class DailyLessonScheduleModel
+{
+    public DayOfWeek DayOfWeek { get; set; }
+    public string CourseName { get; set; }
+    public string CourseLevel { get; set; }
+
+    public List<string> TeacherNames { get; set; } = new();
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+
+    public int StudentCount { get; set; }
+}
